6b44fbd5b8f8715a2e227ce0e5313bc43ad5ffae
Aaaaaaaaaaaaagh <47068660+Aaaaaaaaaaaaagh@users.noreply.github.com>
2020-11-21 21:30:21 -0700

    Added some additional pages to UI
    Included pages to have functionality to view and test a number of different items from the SQL database. Expected aras to be tested are the friends list / befriending, as well as the ability to post and upload comments.

--- app/src/main/java/com/example/cse412project/FirstFragment.java
+++ app/src/main/java/com/example/cse412project/FirstFragment.java
@@ -23,22 +23,12 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.LoginBut).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
-
-        view.findViewById(R.id.ToCreateAccountBut).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.firstFragment_To_CreateAccount);
-            }
-        });
-
-
     }
 }
--- app/src/main/java/com/example/cse412project/MainActivity.java
+++ app/src/main/java/com/example/cse412project/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.cse412project;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +22,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     @Override
@@ -37,10 +47,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_Settings) {
-            return true;
-        }
-        if (id == R.id.action_SignOut) {
+        if (id == R.id.action_settings) {
             return true;
         }
 
